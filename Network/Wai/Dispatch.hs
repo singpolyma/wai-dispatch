c1d32ce71fe2e2cf617796b3a7698841291785b3
Stephen Paul Weber <singpolyma@singpolyma.net>
2012-08-08 21:52:25 -0500

    hlint/-Wall clean and documentation

--- Network/Wai/Dispatch.hs
+++ Network/Wai/Dispatch.hs
@@ -2,7 +2,7 @@
 module Network.Wai.Dispatch (dispatch, Route(..), Piece(..)) where
 
 import Data.Maybe (fromMaybe)
-import Network.Wai (Application, Request(pathInfo, requestMethod), Response)
+import Network.Wai (Application, Request(pathInfo, requestMethod))
 import Yesod.Routes.Dispatch (Route(..), Piece(..), toDispatch)
 import Data.Text (toUpper)
 import Data.Text.Encoding (decodeUtf8)
@@ -13,4 +13,4 @@ dispatch :: Application         -- ^ Default 'Application' (likely 404)
 dispatch defaultApp routes env = app env
 	where
 	app = fromMaybe defaultApp (toDispatch routes routeOn)
-	routeOn = (toUpper $ decodeUtf8 $ requestMethod env):(pathInfo env)
+	routeOn = toUpper (decodeUtf8 $ requestMethod env) : pathInfo env
